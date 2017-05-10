d2aa6160a6aafef1d770b8d5987af5d1faf010ba
kimhaeun <gu01056@naver.com>
2017-05-10 12:11:15 +0900

    My, World 출력함

--- src/test/main/MainClass.java
+++ src/test/main/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass {
 		//sysout + ctrl + space_자동단축
 		//콘솔창에 Hello, World 문자열 출력하기
 		System.out.println("Hello, World!");
-		
+		//콘솔창에 My, World 문자열 출력하기
+		System.out.println("My, World!");
 	}
 }
