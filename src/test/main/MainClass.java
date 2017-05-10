1adb0c2a9a58713436b73834b4a9616e5f404e62
kimhaeun <gu01056@naver.com>
2017-05-10 12:07:01 +0900

    Hello, World 출력함

---
+++ src/test/main/MainClass.java
@@ -0,0 +1,13 @@
+//빨간 글씨 = 예약어
+package test.main;
+//MainClass = 식별자/클래스이름
+//test.main package에 속해있는 클래스
+//MainClass.java 소스코드->컴파일->.class로 바뀜
+public class MainClass {
+	public static void main(String[] args){
+		//sysout + ctrl + space_자동단축
+		//콘솔창에 Hello, World 문자열 출력하기
+		System.out.println("Hello, World!");
+		
+	}
+}
